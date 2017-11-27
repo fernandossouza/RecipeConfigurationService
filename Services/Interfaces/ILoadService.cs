5c4d4b63b968a9076f02a32a5139f6a4aea520f4
Fernando Souza Silva <fernandos@spi.local>
2017-11-27 11:35:45 -0200

    Create Service and interface of Load

---
+++ Services/Interfaces/ILoadService.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using recipeconfigurationservice.Model;
+namespace recipeconfigurationservice.Services.Interfaces
+{
+    public interface ILoadService
+    {
+        Task<List<Load>> getLoads(int start,int quantity);
+        Task<Load> getLoad(int loadId);
+
+         Task<Load> addLoad(Load load);
+
+         Task<Load> updateLoad(int loadId,Load load);
+         Task<Load> deleteLoad(int loadId);
+    }
+}
---
+++ Services/LoadService.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using recipeconfigurationservice.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using recipeconfigurationservice.Data;
+using recipeconfigurationservice.Model;
+using System.Threading.Tasks;
+namespace recipeconfigurationservice.Services
+{
+    public class LoadService :ILoadService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LoadService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Load>> getLoads(int startat, int quantity)
+        {
+            var loadId = await _context.Loads
+                     .OrderBy(x => x.loadId)
+                     .Skip(startat).Take(quantity)
+                     .Select(x => x.loadId)
+                     .ToListAsync();
+            List<Load> loads = new List<Load>();
+            foreach (var item in loadId)
+            {
+                var load = await getLoad(item);
+                if (load != null)
+                    loads.Add(load);
+            }
+
+            return loads;
+        }
+
+        public async Task<Load> getLoad(int loadId)
+        {
+            var load = await _context.Loads
+                       .Include(x => x.loadConfiguration)
+                         .ThenInclude(b=>b.sqlLoad)
+                             .ThenInclude(a=>a.parameterLoad)
+                    .Include(x => x.loadConfiguration)
+                        .ThenInclude(x=>x.apiLoad)
+                            .ThenInclude(x=>x.parameterLoad)                    
+                     .FirstOrDefaultAsync();
+
+            
+            return load;
+        }
+
+        public async Task<Load> addLoad(Load load)
+        {
+            _context.Loads.Add(load);
+            await _context.SaveChangesAsync();
+            return load;
+        }
+
+        public async Task<Load> updateLoad(int loadId, Load load)
+        {
+             var loadDb = await _context.Loads
+                       .Include(x => x.loadConfiguration)
+                         .ThenInclude(b=>b.sqlLoad)
+                             .ThenInclude(a=>a.parameterLoad)
+                    .Include(x => x.loadConfiguration)
+                        .ThenInclude(x=>x.apiLoad)
+                            .ThenInclude(x=>x.parameterLoad)                    
+                     .FirstOrDefaultAsync();
+
+
+            if (loadId != loadDb.loadId && loadDb == null)
+            {
+                return null;
+            }
+
+            _context.Loads.Update(load);
+            await _context.SaveChangesAsync();
+            return load;
+        }
+
+        public async Task<Load> deleteLoad(int loadId)
+        {
+            var loadDB = await _context.Loads
+                    .Include(x => x.loadConfiguration)                    
+                     .Where(x => x.loadId == loadId)
+                     .FirstOrDefaultAsync();
+
+
+            if (loadDB != null)
+            {
+                _context.Entry(loadDB).State = EntityState.Deleted;
+                await _context.SaveChangesAsync();
+            }
+            return loadDB;
+        }
+  
+    }
+}
